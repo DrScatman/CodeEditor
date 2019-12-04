9c0c00c81a155ba8c8ee0e0a46b375809b5baaef
DrScatman <milleja1@mail.gvsu.edu>
2019-12-04 12:08:10 -0500

    Fixed adding to tables

--- src/store.js
+++ src/store.js
@@ -5,17 +5,9 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    fileKey: "",
-    fileName: "",
     userEmail: "",
     setUserEmail(email) {
       this.userEmail = email;
-    },
-    setFileKey(newKey) {
-      this.fileKey = newKey;
-    },
-    setFileName(newName) {
-      this.fileKey = newName;
     }
-  },
+  }
 })
